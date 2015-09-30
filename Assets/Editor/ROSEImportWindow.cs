9438ece407d12250f12b420fe0c948458ecf8120
Brett Lawson <brett19@gmail.com>
2015-09-30 20:55:43 -0300

    Added window for specifying user 3DDATA path.
    Also fixed up some missing files.

---
+++ Assets/Editor/ROSEImportWindow.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class ROSEImportWindow : EditorWindow {
+
+    private const string DataPathKey = "ROSE_DataPath";
+
+    private string dataPath = "";
+
+    [MenuItem("ROSE/Import Window")]
+    static void Init() {
+        var window = GetWindow<ROSEImportWindow>();
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+        dataPath = EditorGUILayout.TextField("3DData Path", dataPath);
+    }
+
+    void OnFocus()
+    {
+        if (EditorPrefs.HasKey(DataPathKey))
+        {
+            dataPath = EditorPrefs.GetString(DataPathKey);
+        }
+    }
+
+    void OnLostFocus()
+    {
+        EditorPrefs.SetString(DataPathKey, dataPath);
+    }
+
+    void OnDestroy()
+    {
+        EditorPrefs.SetString(DataPathKey, dataPath);
+    }
+
+    static string GetDataPath() {
+        var window = GetWindow<ROSEImportWindow>();
+        if (window.dataPath != "")
+        {
+            return window.dataPath;
+        }
+        else
+        {
+            return EditorPrefs.GetString(DataPathKey);
+        }
+    }
+}
